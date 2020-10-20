a39194ff15131de2f88ae0acd5f7db40df7ba821
ngwoon <todory2002@gmail.com>
2020-10-21 01:16:13 +0900

    main page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  SERVER = "http://localhost:3000/"
+  retObj = {
+    title: 'Book Directory',
+    menuList: ['1. 문서 편집', '2. 문서 찾기', '3. 로그인'],
+    href: ['edit', 'search', 'login'],
+  }
+  res.render('index', retObj);
 });
 
 module.exports = router;
