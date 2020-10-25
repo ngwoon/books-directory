beebcce8cc709d07ece58d5cb63213be04fa0941
ngwoon <todory2002@gmail.com>
2020-10-25 18:59:33 +0900

    login page design

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-  res.send('login page');
+  res.render('login');
 });
 
 module.exports = router;
