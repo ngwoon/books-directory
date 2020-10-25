8a891d0fc1089b9d860314b2a7fac6bb23217d49
ngwoon <todory2002@gmail.com>
2020-10-26 02:15:56 +0900

    login post 요청

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+router.post('/login', function(req, res, next) {
+  
+})
 
 module.exports = router;
